d1d230e2c772cf121ebc5201bc989713bba186a7
lvgang <lvgang@golaxy.cn>
2019-11-07 18:25:14 +0800

    简单调用redis

--- service-spring-data-redis/src/test/java/com/cat/code/RedisTest.java
+++ service-spring-data-redis/src/test/java/com/cat/code/RedisTest.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.ExecutorService;
@@ -30,6 +31,9 @@ public class RedisTest {
     private RedisTemplate redisTemplate;
     @Autowired
     private ObjectMapper objectMapper = new ObjectMapper();
+
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     public void test1() throws Exception{
